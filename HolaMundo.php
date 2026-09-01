9ce167da818dbbeefa3f776c7de14b68c1ba19fc
AmbarGreco <ambarlorien18@gmail.com>
2026-09-01 10:34:18 -0500

    Add files via upload

---
+++ HolaMundo.php
@@ -0,0 +1,28 @@
+<html>
+
+<head>
+
+    <!-- Título que aparece en la pestaña del navegador -->
+    <title>Ejemplo</title>
+
+</head>
+
+<body>
+
+    <!-- Título principal de la página -->
+    <h1> Ejemplo simple.</h1>
+
+    <!-- Texto de explicación del ejemplo -->
+    primer ejemplo de codigo php embebido dentro de
+    codigo html.<br>
+
+    <?php
+
+    // Muestra el mensaje "Hola, Mundo!" en la página
+    echo "Hola, Mundo!";
+
+    ?>
+
+</body>
+
+</html>
